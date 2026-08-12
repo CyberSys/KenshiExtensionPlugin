46caa235806098a2c6dee85ee7e9adb46b17b87d
Lucius <ellipsecurvexnynzn@gmail.com>
2026-08-12 16:58:53 +0900

    著作権表示を追加

--- kep-core/include/extern/AppearanceBase.h
+++ kep-core/include/extern/AppearanceBase.h
@@ -1,4 +1,12 @@
-﻿#pragma once
+﻿/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Copyright (C) 2025-2026 Lucius
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+#pragma once
 #include <ogre/OgrePrerequisites.h>
 #include <ogre/OgreVector3.h>
 #include <ogre/OgreVector4.h>
